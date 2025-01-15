ef4d1a36f37c145f7704cc758ce0fd4462b100c5
Brenda <brenda@DESKTOP-5I90UFM>
2025-01-15 14:37:19 -0500

    odd even or 0 Jan 15

---
+++ exercise2.c
@@ -0,0 +1,14 @@
+//write a program to determine if a number entered by user is negative, positive or zero
+//write a program to determine if a number entered by user is odd or even
+
+#include <stdio.h>
+
+int main(){
+
+    int userNumber;
+    printf("Enter a number: ");
+    scanf("%d", &userNumber);
+
+    (userNumber < 0) ? printf("The number is negative\n") : (userNumber > 0) ? printf("The number is positive\n") : printf("The number is 0\n");
+    return 0;
+}

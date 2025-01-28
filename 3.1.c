d3b42d1e7fa09fa74f896f88410466cea3a6d0c8
Brenda <brenda@DESKTOP-5I90UFM>
2025-01-27 19:05:28 -0500

    3.1 + cleaning up repo

---
+++ 3.1.c
@@ -0,0 +1,13 @@
+//using pointer, print the second element
+
+#include <stdio.h>
+
+int main() 
+{
+    int numbers[] = {4, 15, 2, 89, 39, 78};
+
+    printf("%d\n", *(numbers + 1));
+
+
+    return 0; 
+}
